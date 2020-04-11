25e12b9ab0728488f6255fb2f2cfbc277a4037fe
Sal <salvadorchaveztrejo@gmail.com>
2020-04-11 08:53:26 -0700

    conditional to atlernate player sprite during character creation

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,7 @@
 var start = $("#startGame")
 var titleScreen = $("#title-link")
 var createChara = $("#chara")
+var charaClass = $(".charaClass")
 
 start.on("click", () => {window.location.replace("/game");})
 
@@ -8,3 +9,18 @@ titleScreen.on("click", () => {window.location.replace("/home");})
 
 createChara.on("click", () => {window.location.replace("/create");})
 
+//$(".custom-select").click( () => console.log($(".custom-select").val()))
+
+$(".custom-select").click( () => {
+    if($(".custom-select").val() === "mercenary"){
+        $("#charaSprite").attr("src", "./assets/game-art/sprites/player/mercenary.png")
+    } else if ($(".custom-select").val() === "knight"){
+        $("#charaSprite").attr("src", "./assets/game-art/sprites/player/knight.png")
+    } else if ($(".custom-select").val() === "mage"){
+        $("#charaSprite").attr("src", "./assets/game-art/sprites/player/mage.png")
+    } else if ($(".custom-select").val() === "assassin"){
+        $("#charaSprite").attr("src", "./assets/game-art/sprites/player/assassin.png")
+    } else {
+        $("#charaSprite").attr("src", "./assets/game-art/sprites/player/default.png")
+    }
+})
