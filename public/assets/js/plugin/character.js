ef52e6565dcb49aa8784741be12a32bfe8a67b96
Sal <salvadorchaveztrejo@gmail.com>
2020-04-13 09:15:43 -0700

    name parameter on character class added

--- public/assets/js/plugin/character.js
+++ public/assets/js/plugin/character.js
@@ -3,17 +3,18 @@ var Unit = new Phaser.Class({
  
     initialize:
  
-    function Unit(scene, x, y, texture, frame, hp, strength, defense, speed) {
-        Phaser.GameObjects.Sprite.call(this, scene, x, y, texture, frame)
+    function Unit(scene, x, y, texture, name, hp, strength, defense, speed) {
+        Phaser.GameObjects.Sprite.call(this, scene, x, y, texture)
+        this.name= name;
         this.maxHp = this.hp = hp;
-        this.strength = strength; // default damage 
+        this.strength = strength;
         this.defense = defense;
         this.speed = speed;             
     },
     attack: function(target) {
         target.takeDamage(this.strength);      
     },
-    takeDamage: function(strength) {
+    damageCalc: function(strength) {
         this.hp -= strength;        
     }
 });
@@ -24,7 +25,6 @@ var Player = new Phaser.Class({
     initialize:
     function Player(scene, x, y, texture, frame, hp, strength, defense, speed) {
         Unit.call(this, scene, x, y, texture, frame,  hp, strength, defense, speed);
-        //this.flipX = true;
         this.setScale(0.6);
     }
 });
